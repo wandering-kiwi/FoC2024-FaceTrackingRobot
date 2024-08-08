e41ce9f948d239deb29145834c5f68f2adad34cd
wandering-kiwi <67548758+wandering-kiwi@users.noreply.github.com>
2024-08-08 10:15:06 +0100

    Update program.py

--- program.py
+++ program.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from motors import *
 
 app = Flask(__name__)
+
+stop = False
 
 @app.route('/')
 def start():
@@ -8,7 +11,18 @@ def start():
 
 @app.route("/coords/<x>/<y>")
 def coords(x, y):
-   print(x, y)
+   scaledX = float(x)/320 - 1
+   if x == "False" or stop:
+       forward(0)
+       return
+   if scaledX > -0.25:
+       print("right")
+       right(abs(scaledX)*60)
+   elif scaledX < 0.25:
+       print("left")
+       left(abs(scaledX)*60)
+   else:
+       forward(0)
    return 'okay'
 
 if __name__=='__main__':
